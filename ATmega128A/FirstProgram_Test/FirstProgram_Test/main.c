3d9fad93ee874f3f24763962454b7c69b3b9a12a
서호준 <henryseo1000@gmail.com>
2024-04-02 02:56:32 +0900

    ATmega128A - Test

---
+++ ATmega128A/FirstProgram_Test/FirstProgram_Test/main.c
@@ -0,0 +1,22 @@
+/*
+ * main.c
+ *
+ * Created: 4/2/2024 2:25:41 AM
+ *  Author: henry
+ */ 
+
+// 새 프로젝트 생성 시 솔루션 탐색기에 현재 솔루션과 프로젝트를 트리 형태로 보여줌
+#define F_CPU 16000000L
+#include <xc.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRA |= 0x01; 
+    while(1)
+    {
+        //TODO:: Please write your application code 
+		
+    }
+}
---
+++ ATmega128A/FirstProgram_Test/SecondProgram_Test/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 4/2/2024 2:27:52 AM
+ *  Author: henry
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
