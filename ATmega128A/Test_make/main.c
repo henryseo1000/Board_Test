bbaa0992d05d7f2612de7a7efb2d0a076db59441
서호준 <henryseo1000@gmail.com>
2024-04-02 04:14:37 +0900

    ATmega128a - Serial Test Success

--- ATmega128A/Test_make/main.c
+++ ATmega128A/Test_make/main.c
@@ -1,11 +1,13 @@
-
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main(){
     DDRA |= 0x01;
     while(1){
-
+        PORTA |= 0x01;
+        _delay_ms(1000);
+        PORTA &= ~0x01;
+        _delay_ms(1000);
     }
     return 0;
 }
